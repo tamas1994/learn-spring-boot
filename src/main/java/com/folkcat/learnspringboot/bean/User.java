b460b7d964e2b63d8e35b2907d2b6ccd7b827782
Tamas <donghaifeng@hotmail.com>
2017-06-27 10:53:21 +0800

    First commit

---
+++ src/main/java/com/folkcat/learnspringboot/bean/User.java
@@ -0,0 +1,34 @@
+package com.folkcat.learnspringboot.bean;
+
+/**
+ * Created by Tamas on 2017/6/27.
+ */
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
