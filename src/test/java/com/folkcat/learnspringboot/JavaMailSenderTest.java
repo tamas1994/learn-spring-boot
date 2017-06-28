e80a4f4b55f65a7557406f5e79708b2b5053d389
Tamas <donghaifeng@hotmail.com>
2017-06-28 14:49:35 +0800

    新增JavaMailSender测试用例

---
+++ src/test/java/com/folkcat/learnspringboot/JavaMailSenderTest.java
@@ -0,0 +1,30 @@
+package com.folkcat.learnspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * Created by Tamas on 2017/6/28.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class JavaMailSenderTest {
+    @Autowired
+    private JavaMailSender mailSender;
+    @Test
+    public void sendSimpleMail() throws Exception {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("tamas***@163.com");
+        message.setTo("67337****@qq.com");
+        message.setSubject("主题：简单邮件");
+        message.setText("测试邮件内容");
+        mailSender.send(message);
+    }
+}
