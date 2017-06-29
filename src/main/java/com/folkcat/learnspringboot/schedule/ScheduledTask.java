fca763efe0e01c0b14646d11740adeb818a546ae
Tamas <donghaifeng@hotmail.com>
2017-06-29 09:14:10 +0800

    用@Scheduled创建定时任务

---
+++ src/main/java/com/folkcat/learnspringboot/schedule/ScheduledTask.java
@@ -0,0 +1,19 @@
+package com.folkcat.learnspringboot.schedule;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Tamas on 2017/6/29.
+ */
+@Component
+public class ScheduledTask {
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        System.out.println("现在时间：" + dateFormat.format(new Date()));
+    }
+}
