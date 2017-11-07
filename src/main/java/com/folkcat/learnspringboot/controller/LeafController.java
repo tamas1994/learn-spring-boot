6c671ddbc3bcb473f2c0faddab7734d2977b6b9e
Tamas <donghaifeng@hotmail.com>
2017-11-07 16:45:39 +0800

    添加thyme demo

---
+++ src/main/java/com/folkcat/learnspringboot/controller/LeafController.java
@@ -0,0 +1,19 @@
+package com.folkcat.learnspringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+/**
+ * Created by Tamas on 2017/11/7.
+ */
+@Controller
+@RequestMapping(value="/leaf")
+public class LeafController {
+    @RequestMapping("index")
+    public String indexHtml(Map<String, Object> map){
+        map.put("hello", "this is a thymeleaf test");
+        return "/firstleaf";
+    }
+}
